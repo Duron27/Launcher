6c320c9aaca1fcbef1d7dd7bf1ad3a2a45b7a1c3
Duron27 <duron27@gmail.com>
2024-03-10 22:03:54 -0400

    working directory tree
    yeppers

--- app/src/main/java/com/libopenmw/android/ui/home/HomeFragment.kt
+++ app/src/main/java/com/libopenmw/android/ui/home/HomeFragment.kt
@@ -1,10 +1,14 @@
 package com.libopenmw.android.ui.home
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.libopenmw.android.databinding.FragmentHomeBinding
@@ -12,18 +16,16 @@ import com.libopenmw.android.databinding.FragmentHomeBinding
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private lateinit var homeViewModel: HomeViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+        homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
@@ -32,7 +34,33 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        val pickDirectoryButton: Button = binding.pickDirectoryButton
+        val selectedDirectoryTextView: TextView = binding.selectedDirectoryTextView
+
+        pickDirectoryButton.setOnClickListener {
+            openDirectoryPicker()
+        }
+
         return root
+    }
+
+    private val directoryPickerActivityResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val selectedDirectoryUri = result.data?.data
+                selectedDirectoryUri?.let { uri ->
+                    val selectedDirectoryPath = uri.path
+                    val selectedDirectoryTextView: TextView = binding.selectedDirectoryTextView
+                    selectedDirectoryTextView.text = selectedDirectoryPath
+                    selectedDirectoryTextView.visibility = View.VISIBLE
+                }
+            }
+        }
+
+    private fun openDirectoryPicker() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE)
+        directoryPickerActivityResult.launch(intent)
     }
 
     override fun onDestroyView() {
